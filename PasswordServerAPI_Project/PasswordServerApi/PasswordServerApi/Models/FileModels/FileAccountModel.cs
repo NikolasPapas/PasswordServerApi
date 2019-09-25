f0d42f2fd34415754de1e12ea1865e2228b33177
nikolas <nikolaspapazian@gmail.com>
2019-09-25 14:37:42 +0300

    add Gitignore and FileReader

---
+++ PasswordServerAPI_Project/PasswordServerApi/PasswordServerApi/Models/FileModels/FileAccountModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace PasswordServerApi.Models.FileModels
+{
+	[DataContract]
+	public class FileAccountModel
+	{
+		[DataMember(Name = "accountId")]
+		public string AccountId { get; set; }
+
+		[DataMember(Name = "firstName")]
+		public string FirstName { get; set; }
+
+		[DataMember(Name = "lastName")]
+		public string LastName { get; set; }
+
+		[DataMember(Name = "userName")]
+		public string UserName { get; set; }
+
+		[DataMember(Name = "email")]
+		public string Email { get; set; }
+
+		[DataMember(Name = "sex")]
+		public string Sex { get; set; }
+
+		[DataMember(Name = "lastLogIn")]
+		public string LastLogIn { get; set; }
+
+
+		//PRIVATE
+		[DataMember(Name = "password")]
+		public string Password { get; set; }
+
+		[DataMember(Name = "passwords")]
+		public List<string> PasswordsIds { get; set; }
+
+	}
+}
---
+++ PasswordServerAPI_Project/PasswordServerApi/PasswordServerApi/ReadFile/FileReader.cs
@@ -0,0 +1,71 @@
+﻿using PasswordServerApi.Models.FileModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasswordServerApi.ReadFile
+{
+	public class FileReader
+	{
+		private const string IMPORT_PATH_LOG_IN = "";
+		private const string IMPORT_FILENAME_LOG_IN = "";
+		private readonly char[] FieldDelimiter = new char[] { ';' };
+		private readonly char[] PasswordIdsDelimiter = new char[] { '_' };
+
+		public FileReader()
+		{
+
+		}
+
+		private IEnumerable<string> FindFileForLogIn()
+		{
+			string[] files = Directory.GetFiles(IMPORT_PATH_LOG_IN, IMPORT_FILENAME_LOG_IN);
+			if (files != null && files.Length > 0)
+				return files.ToList();
+			return null;
+		}
+
+		private IEnumerable<FileAccountModel> ReadLognFile(string filename)
+		{
+			List<FileAccountModel> accounts = new List<FileAccountModel>();
+
+			List<string[]> fileRows = ReadDocument(filename);
+			if (fileRows == null || fileRows.Count < 1)
+				throw new Exception($"Error on File parse. Name: {filename} , Lines: {fileRows.Count}.");
+
+			for (int line = 0; line < fileRows.Count; line++)
+				accounts.Add(GetAccountFromFile(fileRows[line]));
+			return accounts;
+		}
+
+		private List<string[]> ReadDocument(string path)
+		{
+			string[] lines = File.ReadAllLines(path);
+			List<string[]> fileRows = new List<string[]>();
+			foreach (string line in lines)
+			{
+				fileRows.Add(line.Split(FieldDelimiter, StringSplitOptions.RemoveEmptyEntries));
+			}
+			return fileRows;
+		}
+
+		private FileAccountModel GetAccountFromFile(string[] row)
+		{
+			return new FileAccountModel()
+			{
+				AccountId = row[0],
+				FirstName = row[1],
+				LastName = row[2],
+				UserName = row[3],
+				Email = row[4],
+				Sex = row[5],
+				LastLogIn = row[6],
+				Password = row[7],
+				PasswordsIds = row[8].Split(PasswordIdsDelimiter, StringSplitOptions.RemoveEmptyEntries).ToList()
+			};
+		}
+
+	}
+}
